637ada404f1ab3274352094471d0629c9accdf96
dennis2535 <56621760+dennis2535@users.noreply.github.com>
2020-01-13 15:28:36 +0800

    Add files via upload

---
+++ SquarePattern.java
@@ -0,0 +1,17 @@
+
+public class SquarePattern {
+    public static void main(String[] args) {
+        int shape = 5;
+        
+        for (int i = 1; i < 6; i++){
+            for (int j = 1; j < 6; j++) {
+                System.out.print(" * ");
+                shape++;
+                
+            }
+            
+            System.out.println();
+        }
+    }
+    
+}
